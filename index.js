8f948df190163aacedbacc85404f18fbf5057ddf
fengb <contact@fengb.info>
2015-11-07 18:17:23 -0600

    Removed dependency on harmony-reflect

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-require('harmony-reflect') // shim the correct Proxy
-
 var path = require('path')
 var moduleResolveAsCaller = require('module-resolve-as-caller')
 
 var requireDirProxy = module.exports = function (targetDir, base) {
-  targetDir = moduleResolveAsCaller(targetDir)
   var proxyCache = {}
+  targetDir = moduleResolveAsCaller(targetDir)
+  base = base || proxyCache
 
-  return new Proxy(base || proxyCache, {
+  return createProxy(base || proxyCache, {
     get: function (target, prop) {
-      if (prop in target) {
-        return target[prop]
+      if (prop in base) {
+        return base[prop]
       }
 
       if (!proxyCache.hasOwnProperty(prop)) {
@@ -27,6 +26,14 @@ var requireDirProxy = module.exports = function (targetDir, base) {
 
       return proxyCache[prop]
     },
+
+    keys: function (target) {
+      return Object.keys(base)
+    },
+
+    getOwnPropertyDescriptor: function (target, prop) {
+      return Object.getOwnPropertyDescriptor(base, prop)
+    },
   })
 }
 
@@ -35,3 +42,19 @@ requireDirProxy.require = function (targetDir) {
   var base = require(targetDir)
   return requireDirProxy(targetDir, base)
 }
+
+var createProxy = (function () {
+  if (typeof Proxy === 'function') {
+    return function (base, handler) {
+      return new Proxy(base, handler)
+    }
+  }
+
+  if (typeof Proxy === 'object' && typeof Proxy.create === 'function') {
+    return function (base, handler) {
+      return Proxy.create(handler, base)
+    }
+  }
+
+  throw new Error('proxies not supported on this platform. On v8/node/iojs, make sure to pass the --harmony_proxies flag')
+})()
